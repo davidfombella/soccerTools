6bf3a841a6bb7208a9b06eb1e3f263835d0fbbce
David Fombella <dpombal@hotmail.com>
2019-07-10 22:40:17 +0200

    test

---
+++ R/test.R
@@ -0,0 +1,10 @@
+# test
+#
+#
+#   Build and Reload Package:  'Ctrl + Shift + B'
+#   Check Package:             'Ctrl + Shift + E'
+#   Test Package:              'Ctrl + Shift + T'
+
+test <- function() {
+  qplot(mpg, wt, data = mtcars)
+}
